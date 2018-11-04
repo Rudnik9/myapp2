a405e832d94e405e49365f7f84f9c15ecf38c3e9
kkolpa <kkolpa04@gmail.com>
2018-11-04 12:19:58 +0100

    5.2 applying product search and styling edit form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :orders
   scope :has_image, -> { where.not(image_url: nil) }
+
+  def self.search(search_term)
+    Product.where("name ilike ?", "%#{search_term}%")
+  end
+
 end
