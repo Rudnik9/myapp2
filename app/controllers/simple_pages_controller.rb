069856a829e029e5cdb8f17e5dd1866b34869472
kkolpa <kkolpa04@gmail.com>
2018-11-05 09:39:55 +0100

    5.3 adding contact form

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -5,4 +5,13 @@ class SimplePagesController < ApplicationController
     @featured_product = Product.first
     @products = Product.has_image.limit(3)
   end
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 'pawellrudnicki@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+end
 end
